b26480eef9d57db3ed12cab9b967f152ba05f04d
Guilherme Miranda <saguilherme16@gmail.com>
2025-08-09 18:35:23 -0300

    FEAT: add main content section with title and description to the dashboard

--- app.py
+++ app.py
@@ -39,3 +39,7 @@ df_filtrado = df[
     (df['contrato'].isin(contratos_selecionados)) &
     (df['tamanho_empresa'].isin(tamanhos_selecionados))
 ]
+
+# --- Conteúdo Principal ---
+st.title("🎲 Dashboard de Análise de Salários na Área de Dados")
+st.markdown("Explore os dados salariais na área de dados nos últimos anos. Utilize os filtros à esquerda para refinar sua análise.")
